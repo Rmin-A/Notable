af4f74a801b43ed822e02902f29d6f01dfbb0932
Armin <armin.ansari@gmail.com>
2019-02-10 16:30:10 -0500

    rails react restful api rendered, welcome to notable

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,90 @@
 const path = require('path');
 
-module.export = {
-    "mode": "development",
-    "entry": "frontend/components/notable.js",
-    "output": {
-        "path": __dirname+'/app/assets/javascript/bundle.js',
-        "filename": "[name].[chunkhash:8].js"
-    },
-    "module": {
-        "rules": [
-            {
-                "enforce": "pre",
-                "test": /\.(js|jsx)$/,
-                "exclude": /node_modules/,
-                "use": "eslint-loader"
-            },
-            {
-                "test": /\.(js|jsx)$/,
-                "exclude": /node_modules/,
-                "use": {
-                    "loader": "babel-loader",
-                    "options": {
-                        "presets": [
-                            "env",
-                            "react"
-                        ]
-                    }
-                }
-            },
-            {
-                "test": /\.css$/,
-                "use": [
-                    "style-loader",
-                    "css-loader"
-                ]
-            }
-        ]
-    }
+module.exports = {
+  context: __dirname,
+  entry: './frontend/notable.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env', '@babel/react']
+          }
+        },
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
+
+// const webpack = require('webpack');
+// const path = require('path');
+//
+// const config = {
+//   entry: './frontend/notable.jsx',
+//   output: {
+//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+//     filename: 'bundle.js'
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         use: 'babel-loader',
+//         exclude: /node_modules/
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader'
+//         ],
+//         exclude: /\.module\.css$/
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [
+//           'style-loader',
+//           'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+//         ],
+//         include: /\.module\.css$/
+//       },
+//       {
+//         test: /\.svg$/,
+//         use: 'file-loader'
+//       },
+//       {
+//         test: /\.png$/,
+//         use: [
+//           {
+//             loader: 'url-loader',
+//             options: {
+//               mimetype: 'image/png'
+//             }
+//           }
+//         ]
+//       }
+//     ]
+//   },
+//   resolve: {
+//     extensions: [
+//       '.js',
+//       '.jsx'
+//     ]
+//   },
+//   devServer: {
+//     contentBase: './dist'
+//   }
+// };
+//
+// module.exports = config;
