4836ef408eb2e98ef42a8e5f13d8d0bdb09cee99
Armin <armin.ansari@gmail.com>
2019-02-11 22:18:59 -0500

    Navbar done footer image foxing

--- frontend/components/Greeting/Greeting.jsx
+++ frontend/components/Greeting/Greeting.jsx
@@ -4,8 +4,41 @@ import Navbar from '../Navbar/Navbar_container';
 
 const greeting = () => {
   return(
-    <div>
-      <Navbar />
+    <div
+      className="Greeting">
+
+      <div>
+        <Navbar />
+
+        <div
+          class="Greeting-Demo-Login">
+          <p>For your NOTABLE notes! and more!</p>
+          <button
+            className="Demo-Login">
+            Demo Log in
+          </button>
+        </div>
+
+        <div>
+          <p>hello</p>
+        </div>
+      </div>
+
+      <div
+        className="Greeting-Footer">
+        <p>This is an Evernote clone for practice purposes and not for any commercial use!</p>
+
+        <div>
+          <image
+            src={window.staticImages.github}>
+          </image>
+          <image>
+          </image>
+          <image>
+          </image>
+        </div>
+      </div>
+
     </div>
   );
 };
