be5676d5cd5751c18ad1b755a524e28ff3c00e15
Armin <armin.ansari@gmail.com>
2019-02-13 17:14:14 -0500

    footer fixed

--- frontend/components/Greeting/Greeting.jsx
+++ frontend/components/Greeting/Greeting.jsx
@@ -6,54 +6,49 @@ const greeting = () => {
   return(
     <div
       className="Greeting">
-
       <div>
         <Navbar />
-
+      </div>
+      <div
+        className="Greeting-Demo-Login">
+        <img
+          src={window.staticImages.dl}>
+        </img>
+        <p>For your NOTABLE notes! and more!</p>
+        <button
+          className="Demo-Login">
+          Demo Log in
+        </button>
+      </div>
+      <div
+        className="Greeting-Mid-Panel">
         <div
-          class="Greeting-Demo-Login">
+          className="Greeting-Mid_Panel-Boxes">
           <img
-            src={window.staticImages.dl}>
+            src={window.staticImages.mid1}>
           </img>
-          <p>For your NOTABLE notes! and more!</p>
-          <button
-            className="Demo-Login">
-            Demo Log in
-          </button>
+          <p>Focus on what matters most!</p>
         </div>
-
         <div
-          className="Greeting-Mid-Panel">
-          <div
-            className="Greeting-Mid_Panel-Boxes">
-            <img
-              src={window.staticImages.mid1}>
-            </img>
-            <p>Focus on what matters most!</p>
-          </div>
-          <div
-            className="Greeting-Mid_Panel-Boxes">
-            <img
-              src={window.staticImages.mid2}>
-            </img>
-            <p>Remember everything important</p>
-          </div>
-          <div
-            className="Greeting-Mid_Panel-Boxes">
-            <img
-              src={window.staticImages.mid3}>
-            </img>
-            <p>Stay organized, wherever you are</p>
-          </div>
+          className="Greeting-Mid_Panel-Boxes">
+          <img
+            src={window.staticImages.mid2}>
+          </img>
+          <p>Remember everything important</p>
+        </div>
+        <div
+          className="Greeting-Mid_Panel-Boxes">
+          <img
+            src={window.staticImages.mid3}>
+          </img>
+          <p>Stay organized, wherever you are</p>
         </div>
       </div>
-
       <div
         className="Greeting-Footer">
         <div>
           <p>This is an Evernote clone for practice purposes and not for any commercial use!</p>
         </div>
-
         <div
           className="Greeting-Footer-Icons">
           <img
@@ -66,9 +61,7 @@ const greeting = () => {
             src={window.staticImages.linkedin}>
           </img>
         </div>
-
       </div>
-
     </div>
   );
 };
