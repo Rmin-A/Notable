9b6c7ee8ee5f58756e29fbdfe297a871d4091eda
Armin <armin.ansari@gmail.com>
2019-02-24 11:15:51 -0500

    Editor Container done

--- frontend/reducers/unsaved_changes_reducer.js
+++ frontend/reducers/unsaved_changes_reducer.js
@@ -7,21 +7,21 @@ import {
 } from '../actions/note_actions';
 
 
-const unsavedChangesReducer = (state = { notes: false }, action) => {
+const unsavedChangesReducer = (state = { notes: "false" }, action) => {
   Object.freeze(state);
   let newState;
   switch(action.type) {
     case RECEIVE_NOTE:
-    return merge({}, state, { notes: false });
+    return merge({}, state, { notes: "false" });
     case SET_CURRENT_NOTE:
-    return merge({}, state, { notes: false });
+    return merge({}, state, { notes: "false" });
     case UPDATE_CURRENT_NOTE_BODY:
       newState = Object.assign({}, state);
-      newState.notes = true;
+      newState.notes = "true";
       return newState;
     case UPDATE_CURRENT_NOTE_TITLE:
       newState = Object.assign({}, state);
-      newState.notes = true;
+      newState.notes = "true";
       return newState;
     default:
       return state;
